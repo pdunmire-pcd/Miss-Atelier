6c8360d02e784a3792f779d80061ed141933313b
Jessica H <jessicahebert75@gmail.com>
2026-05-19 15:30:01 -0700

    Create a SQL script and added a table to the schema.

---
+++ scripts/schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE products (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    product_name VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    description TEXT,
+    category VARCHAR(50),
+    image_path VARCHAR(255)
+);
