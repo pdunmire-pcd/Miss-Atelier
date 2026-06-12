5b173e3930378678d2bf20678a963cc93a68f89d
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-06-12 16:00:20 -0700

    Re-organized routers by function

--- src/routers/store.routes.js
+++ src/routers/store.routes.js
@@ -8,20 +8,29 @@ router.get("/", storeController.getStorePage);
 
 // Existing users login 
 router.get("/login", authcontroller.getLogin);
+router.post('/login', authcontroller.postLogin);
 
-router.get("/products", storeController.getProductsPage);
-router.get("/products/:id", storeController.getProductDetailPage);
+// Logout
+router.post('/logout', authcontroller.logout);
 
 // Create a new account
 router.get("/register", authcontroller.getRegister);
+router.post('/register', authcontroller.postRegister);
 
+// Account page
+router.get("/account", authcontroller.getAccount);
+
+// About page
 router.get("/about", storeController.getAboutPage);
+
+// Accessing Products
+router.get("/products", storeController.getProductsPage);
+router.get("/products/:id", storeController.getProductDetailPage);
+
 
 router.get("/search", (req, res) => {
     res.render("search", { title: "Search" });
 });
-
-router.get("/account", authcontroller.getAccount);
 
 router.get("/bag", (req, res) => {
     res.render("bag", { title: "Shopping Bag" });
@@ -30,9 +39,5 @@ router.get("/bag", (req, res) => {
 router.get("/contact", (req, res) => {
     res.render("contact", { title: "Contact Us" });
 });
-
-router.post('/register', authcontroller.postRegister);
-router.post('/login', authcontroller.postLogin);
-router.post('/logout', authcontroller.logout);
 
 export default router;
