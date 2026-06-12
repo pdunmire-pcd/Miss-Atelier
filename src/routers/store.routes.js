fae3fcf4cbe8a345ee95762d46e544d53fa7dcbc
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-06-11 21:00:35 -0700

    wire aith routes for register, login, logout

--- src/routers/store.routes.js
+++ src/routers/store.routes.js
@@ -1,22 +1,19 @@
 import { Router } from "express";
 import * as storeController from  '../controllers/store.controller.js';
+import * as authcontroller from '../controllers/auth.controller.js';
 
 const router = Router();
 
 router.get("/", storeController.getStorePage);
 
 // Existing users login 
-router.get("/login", (req, res) => {
-    res.render("login", { title: "Login" });
-});
+router.get("/login", authcontroller.getLogin);
 
 router.get("/products", storeController.getProductsPage);
 router.get("/products/:id", storeController.getProductDetailPage);
 
 // Create a new account
-router.get("/register", (req, res) => {
-    res.render("register", { title: "Create Account" });
-});
+router.get("/register", authcontroller.getRegister);
 
 router.get("/about", storeController.getAboutPage);
 
@@ -36,5 +33,9 @@ router.get("/bag", (req, res) => {
 router.get("/contact", (req, res) => {
     res.render("contact", { title: "Contact Us" });
 });
+
+router.post('/register', authcontroller.postRegister);
+router.post('/login', authcontroller.postLogin);
+router.post('/logout', authcontroller.logout);
 
 export default router;
