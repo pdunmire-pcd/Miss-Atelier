a6228a179d0000a51e369d0b129bdbb407f6d7dc
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-06-11 20:41:50 -0700

    fixed bug in user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,21 +1,21 @@
-import bcyrpt from 'bcrypt';
-import { createUser, getUserByEmail} from '..model/user.repo.js';
+import bcrypt from 'bcrypt';
+import { createUser, getUserByEmail} from '../model/user.repo.js';
 
-export async registerUser(email, passowrd) {
+export async function registerUser(email, password) {
     const existing = await getUserByEmail(email);
     if(existing) {
         throw new Error('User already registered');
     }
-    const passwordHash = await bcyrpt.hash(password, 10);
+    const passwordHash = await bcrypt.hash(password, 10);
     return await createUser(email, passwordHash);
 }
 
-export async function LoginUser(email,password) {
+export async function loginUser(email,password) {
     const user = await getUserByEmail(email);
     if(!user) {
         throw new Error('Invalid email or password');
     }
-    const match = await bcyrpt.compare(password, user.password_hash);
+    const match = await bcrypt.compare(password, user.password_hash);
     if(!match) {
         throw new Error('Invalid email or password');
     }
